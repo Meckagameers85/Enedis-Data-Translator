fe8cc713bc977cf6891d4a17eada3d50357ad8a3
Meckagameers85 <vinetthomas85@gmail.com>
2024-01-17 18:48:21 +0100

    V2
    Traduction avec un fichier Json no foramter

--- Main.py
+++ Main.py
@@ -4,15 +4,18 @@ path = "C:/Users/Thomas/Downloads/ENEDIS_R63_P_CdC_M05PVFR7_00001_20240116121524
 exit_path = "C:/Users/Thomas/Downloads/ENEDIS_R63_P_CdC_M05PVFR7_00001_20240116121524_1.csv"
 
 def get_data(path):
-    data = []
+    data = ""
     Exit_Data = [["Horodate","Valeur"]]
     with open(path, "r") as file:
-        for line in file:
-            data.append(line.strip())
-
-    for i in range (len(data)):
+        data = file.read()
+    data = data.split('"points":')[1]
+    data = data.replace("]}]}]}","]")
+    data = data.replace("}, {","},{")
+    data = data.replace("} ,{","},{")
+    datas = data.split("},{")
+    for i in range (len(datas)):
         ligne = []
-        ligne = data[i].split(",")
+        ligne = datas[i].split(",")
         ligne = ligne[:2]
         Exit_Data.append([ligne[1].split('"')[3],ligne[0].split('"')[3]])
     return Exit_Data
